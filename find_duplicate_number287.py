ea024400599ac61255f5ae52f525f22e618d718f
yang <yang@yangdeMBP.lan>
2016-10-27 12:09:28 +0800

    287

---
+++ find_duplicate_number287.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for num in nums:
+            res ^= num
+        return res
